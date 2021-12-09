bddfa18143e0adc5f17ddf7e9a2314c80cf0c845
DesPetrikov <desik1603@gmail.com>
2021-12-09 12:48:34 +0300

    feat: add solution to task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,42 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const numberMatrix = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    numberMatrix.push(new Array(matrix[0].length).fill(0));
+  }
+  for (let i = 0; i < matrix[0].length; i += 1) {
+    for (let j = 0; j < matrix.length; j += 1) {
+      if (matrix[j][i]) {
+        if (matrix[j - 1] !== undefined && matrix[j - 1][i] !== undefined) {
+          numberMatrix[j - 1][i] = numberMatrix[j - 1][i] + 1;
+        }
+        if (matrix[j - 1] !== undefined && matrix[j - 1][i + 1] !== undefined) {
+          numberMatrix[j - 1][i + 1] = numberMatrix[j - 1][i + 1] + 1;
+        }
+        if (matrix[j] !== undefined && matrix[j][i + 1] !== undefined) {
+          numberMatrix[j][i + 1] = numberMatrix[j][i + 1] + 1;
+        }
+        if (matrix[j + 1] !== undefined && matrix[j + 1][i + 1] !== undefined) {
+          numberMatrix[j + 1][i + 1] = numberMatrix[j + 1][i + 1] + 1;
+        }
+        if (matrix[j + 1] !== undefined && matrix[j + 1][i] !== undefined) {
+          numberMatrix[j + 1][i] = numberMatrix[j + 1][i] + 1;
+        }
+        if (matrix[j + 1] !== undefined && matrix[j + 1][i - 1] !== undefined) {
+          numberMatrix[j + 1][i - 1] = numberMatrix[j + 1][i - 1] + 1;
+        }
+        if (matrix[j] !== undefined && matrix[j][i - 1] !== undefined) {
+          numberMatrix[j][i - 1] = numberMatrix[j][i - 1] + 1;
+        }
+        if (matrix[j - 1] !== undefined && matrix[j - 1][i - 1] !== undefined) {
+          numberMatrix[j - 1][i - 1] = numberMatrix[j - 1][i - 1] + 1;
+        }
+      }
+    }
+  }
+  return numberMatrix;
 }
 
 module.exports = minesweeper;
