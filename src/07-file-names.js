6645e945c81b43c1633efb35357ac0e8a18abaf4
DesPetrikov <desik1603@gmail.com>
2021-12-09 13:16:56 +0300

    feat: add solution to task 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,19 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  if (!names) return [];
+  for (let i = 0; i < names.length; i += 1) {
+    let counter = 1;
+    for (let j = i + 1; j < names.length; j += 1) {
+      if (names[i] === names[j]) {
+        // eslint-disable-next-line no-param-reassign
+        names[j] += `(${counter})`;
+        counter += 1;
+      }
+    }
+  }
+  return names;
 }
 
 module.exports = renameFiles;
